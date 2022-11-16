790304191606b0a0c208c81c3d259cccadfbcac5
sxliang3093 <113310484+sxliang3093@users.noreply.github.com>
2022-11-15 20:43:50 -0500

    Update Person.cpp

--- Person.cpp
+++ Person.cpp
@@ -19,18 +19,32 @@ using namespace std;
 
 Person::Person(string inputString) : Person() {
     //TODO: Implement non-default constructor
+    stringstream ss(inputString);
+    char temporary;
+    ss >> turn >> temporary >> currentFloor;
+    ss >> temporary >> targetFloor;
+    ss >> temporary >> angerLevel;
 }
 
 bool Person::tick(int currentTime) {
     //TODO: Implement tick
-
-    //Returning false to prevent compilation error
-    return false;
+    if (currentTime % TICKS_PER_ANGER_INCREASE == 0 ) {
+        angerLevel += 1;
+    }
+    if (angerLevel == MAX_ANGER) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 void Person::print(ostream &outs) {    
     //TODO: Implement print
+    outs << "f" << currentFloor << "t" << targetFloor << "a" << angerLevel;
+    
 }
+
 
 //////////////////////////////////////////////////////
 ////// DO NOT MODIFY ANY CODE BENEATH THIS LINE //////
